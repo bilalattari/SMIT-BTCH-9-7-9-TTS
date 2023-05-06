b9bec8de14073e4498c646b3943e13c5180394a5
Bilal raza <attari1235@gmail.com>
2023-05-06 19:58:03 +0500

    Switch added

---
+++ Switch/index.js
@@ -0,0 +1,35 @@
+
+
+
+// We learned Switch Statement
+
+/*
+function giveMeColor(status) {
+    var color = ''
+    switch (status) {
+        case "pending":
+            color = 'yellow'
+            break;
+        case "review":
+            color = 'orange'
+            break;
+        case "onway":
+            color = 'blue'
+            break;
+        case "rejected":
+            color = 'rejected'
+            break;
+        default:
+            color = 'green'
+    }
+    return color
+} */
+
+console.log(giveMeColor('onway'))
+
+// var abcgrade = checkUserGrade(80)
+// console.log('abcgrade->', abcgrade)
+// var fail = checkUserGrade(40)
+// console.log('fail->', fail)
+
+
