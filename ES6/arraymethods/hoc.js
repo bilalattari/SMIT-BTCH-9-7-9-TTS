a8e8a271a5e3e7a08d16735883e3c967ed91a628
bilalattari <attari1235@gmail.com>
2023-06-17 19:13:35 +0500

    hoc added

---
+++ ES6/arraymethods/hoc.js
@@ -0,0 +1,33 @@
+const numbers = [4, 8, 10, 3]
+
+const square = num => num * num
+const triple = num => num * num * num
+const addTwo = num => num + 2
+
+const calculate = function (numbers, logic) {
+  let output = []
+  for (var i = 0; i < numbers.length; i++) {
+    output.push(logic(numbers[i]))
+  }
+  return output
+}
+
+console.log('calculateSquare->', numbers.map(square))
+// console.log('calculateTriple->', calculate(triple))
+// console.log('addTwoNumbers->', calculate(addTwo))
+
+// const calculateTriple = function () {
+//   let output = []
+//   for (var i = 0; i < numbers.length; i++) {
+//     output.push(numbers[i] * numbers[i] * numbers[i])
+//   }
+//   return output
+// }
+
+// const addTwoNumbers = function () {
+//   let output = []
+//   for (var i = 0; i < numbers.length; i++) {
+//     output.push(numbers[i] + 2)
+//   }
+//   return output
+// }
