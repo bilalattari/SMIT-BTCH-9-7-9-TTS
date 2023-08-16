67006b96ba1cc413175c1a818588f9b1716bb056
bilalattari <attari1235@gmail.com>
2023-08-16 09:20:50 +0500

    use callback and usehook updated

--- Nextjs/smitts/src/app/page.js
+++ Nextjs/smitts/src/app/page.js
@@ -1,51 +1,15 @@
 'use client'
 import Card from '@/components/Card'
 import Image from 'next/image'
-import { useState } from 'react'
-const product = [
-  {
-    img:
-      'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
-    category: 'watch',
-    name: 'Watch'
-  },
-  {
-    img:
-      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
-    category: 'phonne',
-    name: 'Head Phone'
-  },
-  {
-    img:
-      'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
-    category: 'camera',
-    name: 'Camera'
-  },
-  {
-    img:
-      'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
-    category: 'watch',
-    name: 'Watch'
-  },
-  {
-    img:
-      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
-    category: 'phonne',
-    name: 'Head Phone'
-  },
-  {
-    img:
-      'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
-    category: 'camera',
-    name: 'Camera'
-  }
-]
+import { useState, useMemo, useCallback, memo } from 'react'
 
 export default function Home () {
   const [todos, setTodos] = useState([])
   const [todo, setTodo] = useState('')
   const [isEdit, setIsEdit] = useState(false)
   const [editIndex, setEditIndex] = useState(null)
+
+  const randomNumber = useMemo(() => Math.random() * 100000000, [todos])
 
   const handleOnChangeTodo = e => {
     setTodo(e.target.value)
@@ -59,18 +23,18 @@ export default function Home () {
     setTodo('')
   }
 
-  const deleteTodo = ind => {
+  const deleteTodo = useCallback(ind => {
     let todoUpdate = todos
     todoUpdate.splice(ind, 1)
     setTodos([...todoUpdate])
-  }
+  }, [])
 
-  const onEdit = ind => {
+  const onEdit = useCallback(ind => {
     console.log(todos[ind])
     setTodo(todos[ind])
     setEditIndex(ind)
     setIsEdit(true)
-  }
+  }, [])
 
   const editTodo = () => {
     let todoUpdate = todos
@@ -81,10 +45,13 @@ export default function Home () {
     setTodo('')
   }
 
+  // const todosToShow = useMemo(() => todos, [todos])
+
   return (
     <main className='flex min-h-screen flex-col items-center p-2'>
       <h1 className='text-3xl font-bold m-3'>Todo App Using Next js</h1>
 
+      <h1 className='text-3xl font-bold m-3'>{randomNumber}</h1>
       <div
         className={
           'flex border-slate-500 bg-white p-3 rounded-md  my-2 w-[500px] items-center	 justify-between'
@@ -112,23 +79,30 @@ export default function Home () {
             key={index}
           >
             <h1>{data}</h1>
-            <span>
-              <button
-                onClick={() => onEdit(index)}
-                className={'p-3 m-2 bg-sky-300 rounded-md'}
-              >
-                Edit
-              </button>
-              <button
-                onClick={() => deleteTodo(index)}
-                className={'p-3 m-2 bg-red-600 text-white rounded-md'}
-              >
-                Delete
-              </button>
-            </span>
+            <EditDelBtn onEdit={onEdit} ind={index} onDelete={deleteTodo} />
           </div>
         )
       })}
     </main>
   )
 }
+
+const EditDelBtn = memo(({ onEdit, onDelete, ind }) => {
+  console.log('Render Edit Delete Component')
+  return (
+    <span>
+      <button
+        onClick={() => onEdit(ind)}
+        className={'p-3 m-2 bg-sky-300 rounded-md'}
+      >
+        Edit
+      </button>
+      <button
+        onClick={() => onDelete(ind)}
+        className={'p-3 m-2 bg-red-600 text-white rounded-md'}
+      >
+        Delete
+      </button>
+    </span>
+  )
+})
